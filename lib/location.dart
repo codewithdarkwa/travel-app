1402543e693615edd6a72b4a53708856f320a919
codewithdarkwa <jnrdrkw1@gmail.com>
2023-01-15 18:52:12 +0000

    fix a bug

--- lib/location.dart
+++ lib/location.dart
@@ -1,8 +1,11 @@
 class Location {
   final String name, description, image;
 
-  Location(
-      {required this.name, required this.image, required this.description});
+  Location({
+    required this.name,
+    required this.image,
+    required this.description,
+  });
 }
 
 List<Location> location = [
